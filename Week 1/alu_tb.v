e28aceebca9eeb0128173be564deb442ce458150
Ashok D'sa <109778122+Ashokdsa@users.noreply.github.com>
2024-09-19 14:48:03 +0530

    Update alu_tb.v

--- Week 1/alu_tb.v
+++ Week 1/alu_tb.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: SJEC
+// Engineer: Ashok
 // 
 // Create Date: 18.09.2024 11:51:34
-// Design Name: 
+// Design Name: ALU Test Bench
 // Module Name: alu_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// Project Name: Taamra
+// Target Devices: Simulation
+// Tool Versions: Xilinx Vivado 2018
 // Description: 
 // 
 // Dependencies: 
@@ -30,18 +30,23 @@ reg [3:0]sel;
 wire flag;
 alu d1(a,b,cin,sel,cout,flag,sum,mult);
 initial begin
+    
     for (a=0;a<32'd4294967295;)
     begin
         a=a+1;
+        #2;
         for (b=0;b<32'd4294967295;)
         begin
             b=b+1;
+            #2;
             for (cin=0;cin<1'b1;)
             begin
                 cin=cin+1;
+                #2;
                 for(sel=0;sel<8;)
                 begin
                     sel=sel+1;
+                    #2;
                     $display("At%dns, A =%d B =%d Cin = %d\nSel = %d Sign = %d Out = %d Mult = %d",$time,a,b,cin,sel,flag,sum,mult);
                 end
             end
